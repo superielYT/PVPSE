d0fb2627b4678144eb842729ea914ff990ef403b
Superiel <superielyt@gmail.com>
2026-07-26 11:01:43 +0200

    Add premium animation framework

---
+++ src/main/java/com/codex/pvphud/animation/SlideAnimation.java
@@ -0,0 +1,7 @@
+package com.codex.pvphud.animation;
+
+public final class SlideAnimation extends Animation {
+    public SlideAnimation(float initial) {
+        super(initial, 240L, Easing.OUT_CUBIC);
+    }
+}
