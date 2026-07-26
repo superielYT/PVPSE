3b94496984e143c72f69229d4e3420f604c1087e
Superiel <superielyt@gmail.com>
2026-07-26 11:01:42 +0200

    Add premium animation framework

---
+++ src/main/java/com/codex/pvphud/animation/ScaleAnimation.java
@@ -0,0 +1,7 @@
+package com.codex.pvphud.animation;
+
+public final class ScaleAnimation extends Animation {
+    public ScaleAnimation(float initial) {
+        super(initial, 220L, Easing.OUT_BACK);
+    }
+}
